a3aa683d1e75b8016d371e7b8b152f12aa48485b
Apoquinto <A16003312@alumnos.uady.mx>
2021-12-09 11:18:15 -0600

    refactor(Room): Convert public attr to private

--- Zuul_Base/src/Room.java
+++ Zuul_Base/src/Room.java
@@ -1,4 +1,8 @@
 package src;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * A "Room" represents one location in the scenery of the game. It is
  * connected to other rooms via exits. The exits are labelled north,
@@ -6,25 +10,19 @@ package src;
  * to the neighboring room, or null if there is no exit in that direction.
  */
 public class Room {
-    public String description;
-    public Room northExit;
-    public Room southExit;
-    public Room eastExit;
-    public Room westExit;
+    private String description;
+    private HashMap<String, Room> paths;
 
     public Room(String description) {
         this.description = description;
     }
 
-    public void setExits(Room north, Room east, Room south, Room west) {
-        if (north != null)
-            northExit = north;
-        if (east != null)
-            eastExit = east;
-        if (south != null)
-            southExit = south;
-        if (west != null)
-            westExit = west;
+    public Room getExit(String path){
+        return this.paths.get(path);
+    }
+
+    public void setExits(HashMap<String, Room> paths) {
+        this.paths = paths;
     }
 
     public String getDescription() {

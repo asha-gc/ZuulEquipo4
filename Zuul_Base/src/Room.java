b925456b7d5010db538a55a64d8cb6fa75a7b309
Apoquinto <A16003312@alumnos.uady.mx>
2021-12-09 21:33:31 -0600

    refactor(Room): Improve paths reference

--- Zuul_Base/src/Room.java
+++ Zuul_Base/src/Room.java
@@ -1,8 +1,6 @@
 package src;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-
 /**
  * A "Room" represents one location in the scenery of the game. It is
  * connected to other rooms via exits. The exits are labelled north,
@@ -13,9 +11,14 @@ public class Room {
     private String description;
     private HashMap<String, Room> paths;
 
-    public Room(String description) {
-        this.description = description;
+    public Room() {
+        this.description = "";
+        this.paths = new HashMap<String, Room>();
     }
+
+    public Boolean isValidExit(String key){
+        return this.paths.containsKey(key);
+    }  
 
     public Room getExit(String path){
         return this.paths.get(path);
@@ -25,8 +28,16 @@ public class Room {
         this.paths = paths;
     }
 
+    public void setDescription(String desc){
+        this.description = desc;
+    }
+
     public String getDescription() {
         return description;
     }
 
+    @Override
+    public String toString() {
+        return "["+this.description+"]";
+    }
 }

75bb1a8745b1e47e1e5259f2f514de9b615228c7
asha-gc <56806594+asha-gc@users.noreply.github.com>
2021-12-10 21:10:33 -0600

    Eliminación de comentarios

--- Zuul_Base/src/Room.java
+++ Zuul_Base/src/Room.java
@@ -3,12 +3,7 @@
 package src;
 
 import java.util.HashMap;
-/**
- * A "Room" represents one location in the scenery of the game. It is
- * connected to other rooms via exits. The exits are labelled north,
- * east, south, west. For each direction, the room stores a reference
- * to the neighboring room, or null if there is no exit in that direction.
- */
+
 public class Room {
     private String description;
     private HashMap<String, Room> paths;

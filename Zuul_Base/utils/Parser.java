7276e54e781801e3762a1bb93b019bcf660d2619
asha-gc <56806594+asha-gc@users.noreply.github.com>
2021-12-10 20:53:39 -0600

    Update Parser.java

--- Zuul_Base/utils/Parser.java
+++ Zuul_Base/utils/Parser.java
@@ -1,18 +1,8 @@
+/*.Publicado por ArcodeTeam: Jonathan, Roberto y Ashanty el 10 de Diciembre de 2021*/
+
 package utils;
 import java.util.Scanner;
 
-/**
- * 
- * This parser reads user input and tries to interpret it as an "Adventure"
- * command. Every time it is called it reads a line from the terminal and
- * tries to interpret the line as a two word command. It returns the command
- * as an object of class Command.
- *
- * The parser has a set of known command words. It checks user input against
- * the known commands, and if the input is not one of the known commands, it
- * returns a command object that is marked as an unknown command.
- * 
- */
 public class Parser {
     private CommandWords commands;
     private Scanner reader;
